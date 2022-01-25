b01509ea76ce8a9330f9f09c59d1bf9a259f8826
ItachiUchiha5277 <5277.stkabirdin1@gmail.com>
2022-01-26 00:26:33 +0530

    fixed a few bugs

--- JS/BMI.js
+++ JS/BMI.js
@@ -1,13 +1,30 @@
-let height = parseInt(document.getElementById('bmiHeight').value);
-let weight = parseInt(document.getElementById('bmiWeight').value);
+let height = parseFloat(document.getElementById('bmiHeight').value);
+let weight = parseFloat(document.getElementById('bmiWeight').value);
 let output = document.getElementById('bmiResult');
 let fields = document.querySelectorAll('.bmiField');
-let bmi = weight / (height / 100 * height / 100);
-let final = parseInt(bmi.toFixed(2));
+let bmi = weight / ((height / 100) ** 2);
+let final = parseFloat(bmi.toFixed(2));
 
-fields.forEach(function (element) {
-    element.addEventListener('input', function () {
-        output.style.display = 'block';
-        output.innerText = "Your BMI is : " + final;
-    });
-});
+// fields.forEach(function (element) {
+//     element.addEventListener('input', function () {
+//         output.style.display = 'block';
+//         output.innerText = "Your BMI is : " + final;
+//     });
+// });
+function updateValue(event) {
+    event.textContext = event.target.value
+    let height = parseFloat(document.getElementById('bmiHeight').value);
+    let weight = parseFloat(document.getElementById('bmiWeight').value);
+    let bmi = weight / ((height / 100) ** 2);
+    let final = parseFloat(bmi.toFixed(2));
+
+    if (isNaN(final)) {
+        output.innerText = 'Enter values to see the result.'
+    }
+    else {
+        output.innerText = `Your BMI is ${final}`
+    }
+}
+fields.forEach((field) => {
+    field.addEventListener('input', updateValue)
+})
--- JS/Tabs.js
+++ JS/Tabs.js
@@ -1,8 +1,23 @@
+let calctype = document.getElementById('calc-type')
+
+function capitalise (word) {
+    return word[0].toUpperCase() + word.slice(1)
+}
+
+/* makes the age calculator as the default one. 
+fixing the age calc dissappearing when clicked 2 times when the website has loaded. */
+
+document.body.onload = () => {
+    document.querySelector('.AGE').classList.add('active-content')
+    document.querySelector('.age').classList.add('active')
+}
+
+// The main tab system
 function onTabClick(event) {
     let activeTabs = document.querySelectorAll('.active')
     let leftContent = document.querySelector(`.${event.target.classList[1]}`.toUpperCase())
     let leftContentAll = document.querySelectorAll('.calc')
-
+    
     activeTabs.forEach((tab) => {
         tab.classList.remove('active')
     })
@@ -13,9 +28,7 @@ function onTabClick(event) {
 
     event.target.classList.add('active')
     leftContent.classList.add('active-content')
-
-    
-    console.log(event.target.classList[1].toUpperCase())
+    calctype.innerText = `The Calc you are using: ${capitalise(event.target.classList[1])}`
 }
 
 const options = document.querySelector('.calc-options')
